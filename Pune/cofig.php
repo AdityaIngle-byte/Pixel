dcf2eae552b1b02f56a7b2068603c4cffdc801b5
Aditya Ingle <84853487+AdityaIngle-byte@users.noreply.github.com>
2021-06-07 22:44:04 +0530

    Create cofig.php
    Added External Config File

---
+++ Pune/cofig.php
@@ -0,0 +1,8 @@
+<?php
+// define(HOST, "localhost");
+// define(USER, "root");
+// define(PASS, "");
+// define(DATAB, "pixel6");
+// $con=mysqli_connect(HOST,USER,PASS,DATAB) or die("DB Not connected");
+$con=mysqli_connect("localhost","root","","pixel6");
+?>
